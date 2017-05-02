0ffee08056ef38592cd282217acaaf86c93c09c1
Parthjhaveri <parthjhaveri209@gmail.com>
2017-05-02 02:11:11 -0400

    added buttons

---
+++ src/components/buttons.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import '../App.css';
+
+class Buttons extends Component {
+  render() {
+    return (
+      <div className="buttons">      
+        
+      <center>
+        <div className="blogbutton">
+          Twitter
+        </div>
+
+        <div className="blogbutton">
+          Astronomy
+        </div>
+      </center>
+
+      </div>
+    );
+  }
+}
+
+export default Buttons;
