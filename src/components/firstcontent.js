6c68ef64f8182d4a8969a597d3d878c6721344d0
Parthjhaveri <parthjhaveri209@gmail.com>
2017-05-02 23:29:57 -0400

    featured blogs

--- src/components/firstcontent.js
+++ src/components/firstcontent.js
@@ -6,7 +6,50 @@ class FirstContent extends Component {
   render() {
     return (
       <div className="firstContent">
+      	<div className="container">
+      		
+      		{
+      			// FEATURED BLOGS
+      		}
 
+	      	<h1 id="featuredblogheading">Featured Blogs</h1>	
+      		<center>
+
+      			<a href="https://futurism.com/new-research-shows-that-time-travel-is-mathematically-possible/" target="_blank">
+	      			<div className="featuredblog" id="widebox">
+	      				<div className="descbox">
+	      					<p>
+	      						New Research shows that time-travel is mathematically possible
+	      					</p>
+	      				</div>
+	      			</div>
+
+	      		</a>
+
+	      		<div className="featuredblog">
+	      			
+	      		</div>
+
+	      		<div className="featuredblog">
+	      			
+	      		</div>
+	      		
+	      		<br/>
+
+	      		<div className="featuredblog">
+	      		</div>
+
+	      		<div className="featuredblog">
+	      		</div>
+
+	      		<div className="featuredblog">
+	      		</div>
+
+	      		<div className="featuredblog">
+	      		</div>
+      		</center>
+
+      	</div>
       </div>
     );
   }
